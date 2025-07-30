3bb0fc3b97b7a12b60b9efcbec8883ab056098d6
Chibom11 <shivamsurroach10@gmail.com>
2025-07-31 00:46:38 +0530

    cors changed again again

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://wild-oasis-memgq8d4e-shivamsurroach10-7381s-projects.vercel.app'],
+    origin: ['http://localhost:5173', 'https://wild-oasis-m45tfqa2z-shivamsurroach10-7381s-projects.vercel.app'],
     credentials: true,
   })
 );
